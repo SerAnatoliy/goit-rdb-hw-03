a69ffcf0ffa5e971f1a4f6e2ae82159158a6971f
SerAnatoliy <anatolii@maximustribe.com>
2024-04-08 20:00:47 +0200

    images

--- Task4.sql
+++ Task4.sql
@@ -3,12 +3,7 @@ count(id)
 FROM mydb.products
 where price >20 and price <100
 
-SELECT 
+/*SELECT 
 count(id)
 FROM mydb.products
-where price between 21 and 99
-
-SELECT 
-count(id)
-FROM mydb.products
-where price not between 21 and 99
+where price between 21 and 99*/
